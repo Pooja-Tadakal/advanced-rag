08ac390df4324762b42814201de7884bbb1727da
Pooja-Tadakal <poojatadakal1993@gmail.com>
2026-07-25 15:33:35 +0530

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -1,39 +1,4 @@
 
-Skip to content
-shrishail07
-Rag_model_2
-Repository navigation
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security and quality
-Insights
-Files
-Go to file
-t
-T
-README.md
-app.py
-config.py
-prompt.py
-rag.py
-requirements.txt
-utils.py
-Rag_model_2
-/prompt.py
-shrishail07
-shrishail07
-Create prompt.py
-7e911af
- · 
-8 hours ago
-
-Code
-
-Blame
-17 lines (11 loc) · 224 Bytes
 PROMPT = """
 You are an AI assistant.
 
@@ -51,4 +16,3 @@ Question:
 
 Answer:
 """
- Rag_model_2/prompt.py at main · shrishail07/Rag_model_2
